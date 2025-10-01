412bd242ff2a46a733e0e2fb11384d6f02fdc143
kazunari-kawata <k0628k@gmail.com>
2025-10-01 16:19:03 +0900

    Comment.tsxがgit上ではcommentになっていたのでgit上での認識を修正

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -24,7 +24,11 @@ async function getPost(id: string) {
   }
 }
 
-export default async function PostPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const post = await getPost(id);
 
